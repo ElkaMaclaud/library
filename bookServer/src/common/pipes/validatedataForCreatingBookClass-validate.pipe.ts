4b221e302e8e3bf8110e87834bee773a930a7e56
ElkaMaclaud <elkamaclaud@gmail.com>
2024-12-23 17:45:12 +0300

    fix: stage of creating a pipeby using class-validate

--- bookServer/src/common/pipes/validatedataForCreatingBookClass-validate.pipe.ts
+++ bookServer/src/common/pipes/validatedataForCreatingBookClass-validate.pipe.ts
@@ -3,7 +3,16 @@ import { CreateBookDto } from "src/book/dto/create.book.dto";
 
 @Injectable()
 export class ValidateDate implements PipeTransform<CreateBookDto> {
-    async transform(value: CreateBookDto, metadata: ArgumentMetadata) {
+    async transform(value: CreateBookDto, { metatype }: ArgumentMetadata) {
+        if (!metatype || !this.toValidate(metatype)) {
+            return value
+        }
+    }
 
+    const
+
+    private toValidate(metatype: Function): boolean {
+        const types: Function[] = [String, Boolean, Number, Array, Object]
+        return !types.includes(metatype)
     }
 }
